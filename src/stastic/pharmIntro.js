e6d6fb14052e40f696940cbcf1b2887d4008f159
John <john791016@gmail.com>
2022-01-17 23:32:58 +0800

    init

---
+++ src/stastic/pharmIntro.js
@@ -0,0 +1,20 @@
+const pharmIntro = [
+  {
+    title: "Gozilla",
+    description:
+      "哥吉拉 藥師（ゴジラ Gojira，英語：Godzilla），為日本東寶株式會社製作的怪獸，日本影史上最悠久，世界影史上公認最經典最有名的怪獸角色。最早的哥吉拉是一部受到美國《金剛》影響的怪獸電影，描述受到輻射污染的海域中出現一隻身高達50公尺的怪獸哥吉拉。哥吉拉後來成為日本類似電影、動畫、漫畫的怪獸原型，對於日本動漫作品的怪獸形象有非常深遠的影響。哥吉拉從1954年推出第一集，到了2004年11月已經足足50年，也在好萊塢舉行了50歲的生日典禮。哥吉拉系列在日本共有28集，但由於在日本觀眾逐漸減少，所以在第28集之後，哥吉拉系列暫停拍攝。傳奇電影公司於2014年製作了第二部美國拍攝的哥吉拉電影《哥吉拉》，導演為加雷斯·愛德華。2019年的續集《哥吉拉 II 怪獸之王》，由麥可·道格堤執導，也是傳奇影業的怪獸宇宙第三部作品。2021年的《哥吉拉大戰金剛》，由亞當·溫高德執導，為傳奇影業的第四部作品。",
+    imageUrl:
+      "https://i.pinimg.com/564x/4b/43/20/4b432007790226db144466022c713146.jpg",
+    time: 1500,
+  },
+  {
+    title: "KING GONG",
+    description:
+      "King Kong pharmacy is a fictional giant monster resembling a gorilla, who has been depicted in films since the 1930s. It is the third film in the King Kong franchise, and the first in the series. The film was directed by Francis Ford Coppola and starred Frank Capra, Meryl Streep, and Albert Brooks. The film was released in 1933, and was the first of the series to be released in the United States. The film was nominated for a Best Picture Oscar in 1933, and was the first of the series to win the Oscar. The film was also the first of the series to be released in the United States.",
+    imageUrl:
+      "https://i.pinimg.com/564x/ea/90/ec/ea90ec0ba04bf4c299c1ef1ef98d1e3f.jpg",
+    time: 1500,
+  },
+];
+
+export default pharmIntro;
